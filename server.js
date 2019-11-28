564f0ebdbbe0ca7f407e4c501a295c3674dbbbb3
puppypicker <o010814@outlook.com>
2019-11-28 01:39:14 -0800

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex');
+const knex = require('knex/types');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -13,7 +13,7 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl: true,
+    ssl: true
   }
 });
 
