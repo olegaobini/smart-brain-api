09564f14f8dba0b226a6999fbf5ad2828f1887a6
puppypicker <o010814@outlook.com>
2019-11-28 01:15:23 -0800

    database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-cubed-02147',
-    user : 'olegaobini',
-    password : '',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
